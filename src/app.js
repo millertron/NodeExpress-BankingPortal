1d08ebaeed0c2f791d2db1eda886ba7c4dbe80a6
Koha Choji <koha.choji@vistair.com>
2019-11-06 10:25:22 +0000

    Creating a Data Access Library

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { accounts, users, writeJSON } = require('./data');
 
 const express = require('express');
 
@@ -10,12 +11,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded( {extended: true }));
-
-const accountData = fs.readFileSync("src/json/accounts.json", { encoding: "UTF8" });
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync("src/json/users.json", { encoding: "UTF8"});
-const users = JSON.parse(userData);
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
@@ -31,9 +26,7 @@ app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance = fromBalance - transferAmount;
     accounts[req.body.to].balance = toBalance + transferAmount;
 
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, "utf8");
-
+    writeJSON();
     res.render('transfer', { message: "Transfer Completed" });
 });
 app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
@@ -45,10 +38,9 @@ app.post('/payment', (req, res) => {
     accounts.credit.balance = creditBalance - amount;
     accounts.credit.available = creditAvailable + amount;
 
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, "utf8");
-
+    writeJSON();
     res.render('payment', { message: "Payment Successful", account: accounts.credit });
 });
+
 
 app.listen(3000, () => console.log('PS Project Running on Port 3000!'));
