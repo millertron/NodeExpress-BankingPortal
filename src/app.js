9b43177a123530ddc8a20629492d99d9aa9ae0bd
Koha Choji <koha.choji@vistair.com>
2019-11-06 09:00:04 +0000

    Const-ify variables

--- src/app.js
+++ src/app.js
@@ -24,25 +24,26 @@ app.get('/credit', (req, res) => res.render('account', { account: accounts.credi
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 app.get('/transfer', (req, res) => res.render('transfer'));
 app.post('/transfer', (req, res) => {
-    let fromBalance = parseInt(accounts[req.body.from].balance, 10);
-    let toBalance = parseInt(accounts[req.body.to].balance, 10);
+    const fromBalance = parseInt(accounts[req.body.from].balance, 10);
+    const toBalance = parseInt(accounts[req.body.to].balance, 10);
     const transferAmount = parseInt(req.body.amount, 10);
 
     accounts[req.body.from].balance = fromBalance - transferAmount;
     accounts[req.body.to].balance = toBalance + transferAmount;
 
-    let accountsJSON = JSON.stringify(accounts, null, 4);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, { encoding: "utf8"});
 
     res.render('transfer', { message: "Transfer Completed" });
 });
 app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
 app.post('/payment', (req, res) => {
-    let creditBalance = parseInt(accounts.credit.balance, 10);
-    let creditAvailable = parseInt(accounts.credit.available, 10);
+    const creditBalance = parseInt(accounts.credit.balance, 10);
+    const creditAvailable = parseInt(accounts.credit.available, 10);
+    const amount = parseInt(req.body.amount, 10);
 
-    accounts.credit.balance = creditBalance - parseInt(req.body.amount, 10);
-    accounts.credit.available = creditAvailable + parseInt(req.body.amount, 10);
+    accounts.credit.balance = creditBalance - amount;
+    accounts.credit.available = creditAvailable + amount;
 
     let accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, { encoding: "utf8"});
